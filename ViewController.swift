c760e44475be8ee735e4af1a6d7d8a741234ad30
rakhmatullinKraken <renrahman@gmail.com>
2022-05-16 17:37:52 +0300

    init commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testKraken
+//
+//  Created by Work on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
